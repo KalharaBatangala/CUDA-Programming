be1369adb76b6379c1ae79706ae5608d0299cfd3
Kalhara Batangala <kalharabatangala@gmail.com>
2025-03-14 14:10:05 +0530

    kernel launch

---
+++ Test1/kernel.cu
@@ -0,0 +1,21 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+__global__ void hello() {
+    
+    // print the block and thread IDs
+    printf("\nThe block ID is %d --- The thread ID is %d", blockIdx.x, threadIdx.x);
+}
+
+
+int main()
+{
+    hello << <2, 8 >> > ();
+
+    return 0;
+}
